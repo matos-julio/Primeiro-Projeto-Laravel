28b86737c7ebf81352803a6ad07544412466aa25
Júlio Matos Cavalcante da Silva <juliosilva@pmni>
2025-08-01 15:28:39 -0300

    Criado primeiro Model(Category) e Controller(HomeController). Alteração na rota da home e integração dinamica com BD

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-// Route::get('/', function () {
-//     return view('home');
-// });
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
-Route::view('/home', 'home')->name('home');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/about', 'about')->name('about');
 Route::view('/article', 'article')->name('article');
