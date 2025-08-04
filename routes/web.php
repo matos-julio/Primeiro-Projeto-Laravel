b8f470100908bd07364b86253d3223f57c28482b
Júlio Matos Cavalcante da Silva <juliosilva@pmni>
2025-08-04 10:56:55 -0300

    criar PostController com exibição individual, rota dinâmica e navegação entre posts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
@@ -8,3 +9,6 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::view('/contact', 'contact')->name('contact');
 Route::view('/about', 'about')->name('about');
 Route::view('/article', 'article')->name('article');
+
+Route::get('posts/{post}', [PostController::class, 'show'])
+->name('post.show');
